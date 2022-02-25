49a01472f362cba1486f0028c2a2db9ed4690f43
Mynul Islam <mynulakash436@gmail.com>
2022-02-25 22:30:50 +0600

    input TakingInput

---
+++ C-Sharp/TakingInput/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//string input = Console.ReadLine();
+//Console.WriteLine(input);
+//string[] parts=input.Split(' ');
+//Console.WriteLine(input);
+/*for(int i=0; i<parts.Length; i++)
+{
+    Console.WriteLine(parts[i]);    
+}*/
+/*foreach (var part in parts)
+{
+    Console.WriteLine(part.Trim());
+}*/
+
+int size = 0;
+
+string sizeInput=Console.ReadLine();
+
+size=int.Parse(sizeInput);
+
+//Console.WriteLine(size);
+if(int.TryParse(sizeInput, out size))
+{
+
+}
+
