051e935a2f9660f4cd049d148a89cdd8fb027c62
Mynul Islam <mynulakash436@gmail.com>
2022-02-27 18:45:08 +0600

    CustomFormat

---
+++ C-Sharp/CustomNumericFormat/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Globalization;
+
+Console.WriteLine("Hello, World!");
+double value;
+
+value = 1.2;
+Console.WriteLine(value.ToString("0.00", CultureInfo.InvariantCulture));
+Console.WriteLine(String.Format(CultureInfo.InvariantCulture,
+                                "{0:0.00}", value));
+// Displays 1.20
+
+Console.WriteLine(value.ToString("00.00", CultureInfo.InvariantCulture));
+Console.WriteLine(String.Format(CultureInfo.InvariantCulture,
+                                "{0:00.00}", value));
+// Displays 01.20
+
+Console.WriteLine(value.ToString("00.00",
+                  CultureInfo.CreateSpecificCulture("da-DK")));
+Console.WriteLine(String.Format(CultureInfo.CreateSpecificCulture("da-DK"),
+                  "{0:00.00}", value));
+// Displays 01,20
+
+value = .086;
+Console.WriteLine(value.ToString("#0.##%", CultureInfo.InvariantCulture));
+Console.WriteLine(String.Format(CultureInfo.InvariantCulture,
+                                "{0:#0.##%}", value));
+// Displays 8.6%
+
+value = 86000;
+Console.WriteLine(value.ToString("0.###E+0", CultureInfo.InvariantCulture));
+Console.WriteLine(String.Format(CultureInfo.InvariantCulture,
+                  "{0:0.###E+0}", value));
+// Displays 8.6E+4
---
+++ C-Sharp/StringManipulationDemo/StringManipulation/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Diagnostics;
+
+namespace StringManipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine();
+            //stringConversion();
+            //Console.WriteLine();
+            //stringEscape();
+            AppendString();      
+        }
+        private static void stringConversion()
+        {
+            string abc = "This is POlice calling";
+            TextInfo curentTextInfo = CultureInfo.CurrentCulture.TextInfo;
+            Console.WriteLine(abc.ToUpper());
+            string result = curentTextInfo.ToTitleCase(abc);
+            Console.WriteLine(result);
+
+        }
+        private static void stringEscape()
+        {
+            string a = "helloo there \\\\ back slash";
+            string b = @"hello \\\ brother...";
+            Console.WriteLine(b);
+        }
+        private static void AppendString()
+        {
+            string fn = "Mynul";
+            string ln = "Islam";
+            string r = string.Format("My name is {0} {1}", fn, ln);
+            Console.WriteLine(r);
+            r =$"Hey my name is {fn} {ln}";
+            Console.WriteLine(r);
+            Stopwatch stop =new Stopwatch();
+        }
+
+    }
+}
