155b007676880aa45812c540b9e29b97988a68f2
Mynul Islam <mynulakash436@gmail.com>
2022-03-01 05:00:22 +0600

    new

--- C-Sharp/Hello/Program.cs
+++ C-Sharp/Hello/Program.cs
@@ -1,14 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-int x = 10;
-
-string name = "Mynul Islam";
-
-Console.WriteLine(name);
-
-
-
-
+﻿
 
 
 
