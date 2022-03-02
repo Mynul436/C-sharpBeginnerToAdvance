83d46c5b6d0fc2dfa4a5f3b789e35bc4cece8dfc
Mynul Islam <mynulakash436@gmail.com>
2022-03-03 03:30:51 +0600

    Problem

---
+++ Assignment1DevSkill/Problem5/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+Console.WriteLine();
+
+Console.WriteLine("Printing the sum of numbers that are in the 2 diagonal positions in 2d Array.");
+Console.WriteLine();
+
+Console.WriteLine("Enter the size of diagonal array: ");
+
+int num;
+num =Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the elements one by one:");
+
+int[,] array = new int[num, num];
+int sum = 0;
+
+for (int i = 0; i < num; i++)
+{
+    for (int j = 0; j < num; j++)
+    {
+        array[i,j] = Convert.ToInt32(Console.ReadLine());
+        if ((i + j) == num - 1)
+        {
+            sum+=array[i,j];
+        }
+    }
+}
+Console.WriteLine();
+
+Console.WriteLine("Here is the 2D array you have entered!!!");
+for (int i = 0; i < num; i++)
+{
+    Console.WriteLine();
+
+    for (int j = 0; j < num; j++)
+    {
+        Console.Write(array[i,j]+" ");
+    }
+}
+Console.WriteLine();
+Console.WriteLine($"And the sum of Second Diagonal numbers is: {sum}");
+Console.WriteLine();
---
+++ C-Sharp/OOP/WaterBottle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    internal class WaterBottle
+    {
+        string color;
+        double capacity;
+        double height;
+        bool capOpen;
+        double content;
+        void OpnenCap()
+        {
+            capOpen = true;
+        }
+        void CloseCap()
+        {
+            capOpen=false;
+        }
+       public void AddWater(double amount)
+        {
+
+        }
+        void RemoveWater(double amount)
+        {
+
+        }
+    }
+}
