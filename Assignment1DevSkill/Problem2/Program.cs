5c8fa2c91ed6e77a3d5b22003d10015621e735b8
Mynul Islam <mynulakash436@gmail.com>
2022-03-03 03:38:55 +0600

    ..

--- Assignment1DevSkill/Problem2/Program.cs
+++ Assignment1DevSkill/Problem2/Program.cs
@@ -1,28 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-//Console.WriteLine("Hello, World!");
-
-
-int[][,] jaggedArray4 = new int[][,]
-            {
-                new int[,] { {1,3}, {5,7} },
-                new int[,] { {0,2}, {4,6}, {8,10} },
-                new int[,] { {11,22}, {99,88}, {0,9} }
-            };
-
-for (int i = 0; i < jaggedArray4.Length; i++)
+﻿string num1 = Console.ReadLine();
+string num2 = Console.ReadLine();
+string total=Add(num1, num2);
+Console.WriteLine(total);
+string Add(string s1, string s2)
 {
+    bool carry = false;
+    string result = string.Empty;
 
-    int count = 0;
+    if (s1.Length < s2.Length)
+        s1 = s1.PadLeft(s2.Length, '0');
+    if (s2.Length < s1.Length)
+        s2 = s2.PadLeft(s1.Length, '0');
 
-    
-    for (int j = 0; j < jaggedArray4[i].GetLength(count); j++)
+    for (int i = s1.Length - 1; i >= 0; i--)
     {
-
-        for (int k = 0; k < jaggedArray4[j].Rank; k++)
-            Console.Write("Jagged_Array4[" + i + "][" + j + ", " + k + "]: "
-                                        + jaggedArray4[i][j, k] + " ");
-        Console.WriteLine();
+        var augend = Convert.ToInt64(s1.Substring(i, 1));
+        var addend = Convert.ToInt64(s2.Substring(i, 1));
+        var sum = augend + addend;
+        sum += (carry ? 1 : 0);
+        carry = false;
+        if (sum > 9)
+        {
+            carry = true;
+            sum -= 10;
+        }
+        result = sum.ToString() + result;
     }
-        count++;
-    Console.WriteLine();
+    if (carry)
+    {
+        result = "1" + result;
+    }
+
+    return result;
 }
