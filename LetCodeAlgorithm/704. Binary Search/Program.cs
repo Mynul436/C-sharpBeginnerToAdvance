b5a19662b0befabbd47fcce3f44e92b356846302
Mynul Islam <mynulakash436@gmail.com>
2022-03-05 14:15:36 +0600

    .

---
+++ LetCodeAlgorithm/704. Binary Search/Program.cs
@@ -0,0 +1,24 @@
+﻿int num;
+num=Convert.ToInt32( Console.ReadLine());
+int[] input=new int[num];
+
+for(int i=0; i<num; i++)
+{
+    input[i]=Convert.ToInt32(Console.ReadLine());
+}
+Array.Sort(input);
+/*
+foreach (int item in input)
+{
+    Console.WriteLine(item);
+}
+*/
+int target;
+target=Convert.ToInt32( Console.ReadLine());
+
+int res = Array.BinarySearch(input, target);
+if (res < 0)
+    Console.WriteLine(-1);
+else
+    Console.WriteLine(res);
+
