4357c3df8096ec12291202961d583f8d76434260
Mynul Islam <mynulakash436@gmail.com>
2022-03-08 12:28:15 +0600

    constructor

---
+++ C-Sharp/Constrructor/Cons.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constrructor
+{
+    internal class Cons
+    {
+        Cons()
+        {
+            Console.WriteLine("Cons has called");
+        }
+        static void Main(string[] args)
+        {
+            // Cons cons = new Cons();
+            
+            consTructor abc =new consTructor();
+        }
+    }
+
+    class consTructor
+    {
+        public consTructor()
+        {
+            Console.WriteLine("Constructor has called");
+        }
+
+
+    }
+}
