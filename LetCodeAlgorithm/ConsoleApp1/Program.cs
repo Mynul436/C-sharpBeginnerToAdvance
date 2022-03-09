18c555bfebe14d6080ecca54fbd5bfaa7fde6ed5
Mynul Islam <mynulakash436@gmail.com>
2022-03-09 19:22:05 +0600

    .

---
+++ LetCodeAlgorithm/ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            num = Convert.ToInt32(Console.ReadLine());
+            int[] input = new int[num];
+
+            for (int i = 0; i < num; i++)
+            {
+                input[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Array.Sort(input);
+            /*
+            foreach (int item in input)
+            {
+                Console.WriteLine(item);
+            }
+            */
+            int target;
+            target = Convert.ToInt32(Console.ReadLine());
+
+            int res = Array.BinarySearch(input, target);
+            if (res < 0)
+                Console.WriteLine(-1);
+            else
+                Console.WriteLine(res);
+
+        }
+    }
+}
---
+++ LetCodeAlgorithm/Problem/Problem7/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+class Problem7
+{
+    static int N ;
+
+   
+    static void rotateClockwise(int[,] arr)
+    {
+
+        
+        for (int i = 0; i <N/ 2; i++)
+        {
+            for (int j = i; j <N - i - 1; j++)
+            {
+
+                int temp = arr[i, j];
+                arr[i, j] = arr[N - 1 - j, i];
+                arr[N - 1 - j, i] = arr[N - 1 - i, N - 1 - j];
+                arr[N - 1 - i, N - 1 - j] = arr[j, N - 1 - i];
+                arr[j, N - 1 - i] = temp;
+            }
+        }
+    }
+
+    
+    static void printMatrix(int[,] arr)
+    {
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < N; j++)
+                Console.Write(arr[i, j] + " ");
+            Console.Write("\n");
+        }
+    }
+
+  
+    public static void Main()
+    {
+      //  
+        Console.WriteLine("Please enter how many row and colum  should have  (row and col must be same): ");
+        N=Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Input only integer 2d array :");
+        int[,] arr = new int[N,N];
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                arr[i, j] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+
+        rotateClockwise(arr);
+        printMatrix(arr);
+    }
+}
