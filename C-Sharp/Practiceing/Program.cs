4d237f196dc02ca6bc7c54a1fb264f6a42a23dec
Mynul Islam <mynulakash436@gmail.com>
2022-03-10 23:21:49 +0600

    .

---
+++ C-Sharp/Practiceing/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+public class c1
+{
+    enum month
+    {
+        jan,feb,march,april
+    }
+   static int a = 10;
+    String name;
+    int year;
+  //  int month;
+    int day;
+    int age;
+
+
+    c1(String name,int age,int day)
+    {
+        this.name = name;
+      //  this.year = year;    
+        this.day = day;
+        this.age = age;
+        
+    }
+    public void Method()
+    {
+        Console.WriteLine(name);
+
+        Console.WriteLine(year);
+        Console.WriteLine(day);
+        Console.WriteLine(age);
+    }
+    public static void Main(String[] args)
+    {
+        Console.WriteLine((int)month.jan);
+        Console.WriteLine(a);
+        c1 obj = new c1("Mynul", 22, 2);
+        obj.Method();
+    }
+}
---
+++ C-Sharp/Problem1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int TestCase;
+            TestCase = Convert.ToInt32(Console.ReadLine());
+            while (TestCase!=0)
+            {
+               int num = Convert.ToInt32(Console.ReadLine());
+                int max;
+                int min;
+         /*   int[] arr = new int[num];
+             for (int i = 0; i < num; i++)
+             {
+                 arr[i] =Convert.ToInt32(Console.ReadLine());
+             }
+         */
+            string[] x = Console.ReadLine().Split(' ');
+            int[]arr = new int[x.Length];
+            for (int i = 0; i < x.Length; i++)
+            {
+                arr[i] = Convert.ToInt32(x[i]);
+            }
+           
+             max= arr.Max();
+             min= arr.Min();
+         /*   max = arr[0];
+            min = arr[0];
+
+            for (int i = 1; i <x.Length; i++)
+            {
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                }
+
+
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                }
+            }*/
+          
+            Console.WriteLine(max - min);
+
+                TestCase--;
+            }
+        }
+    }
+
