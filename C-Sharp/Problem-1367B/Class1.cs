57e993feaf5e7b752aba7c1efc661f2f826cd329
Mynul Islam <mynulakash436@gmail.com>
2022-03-14 04:39:34 +0600

    .

---
+++ C-Sharp/Problem-1367B/Class1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_1367B
+{
+    internal class Class1
+    {
+        public static void Main(String[] args)
+        {
+            int TestCase;
+            TestCase = Convert.ToInt32(Console.ReadLine());
+            while (TestCase-- > 0)
+            {
+                int even = 0;
+                int odd = 0;   
+                int count = 0;
+                int num = Convert.ToInt32(Console.ReadLine());
+                String[] str = Console.ReadLine().Split();
+                int[] arr = new int[str.Length];
+                for (int i = 0; i < str.Length; i++)
+                {
+                    arr[i] = Convert.ToInt32(str[i]);
+                }
+
+                for (int i = 0; i < str.Length; i++)
+                {
+                    // Console.Write(arr[i]+" ");
+
+                    if (arr[i] % 2 != i % 2)
+                    {
+                        if (i % 2 == 0)
+                        {
+                            even++;
+                        }
+                        else
+                        {
+                            odd++;
+                        }
+                    }
+
+                    //  Console.WriteLine();
+
+                    /*        if ((str.Length / 2) <= even && even!=0)
+                           // if (Math.Ceiling((decimal)(str.Length / 2)) <= even && even!=0)
+
+                            {
+
+                                for (int i = 0; i < str.Length; i+=2)
+                                {
+                                    if(arr[i]%2==0 && arr[i + 2] % 2 != 0)
+                                    {
+                                        Console.WriteLine(0);
+                                    }
+
+                                    for(int j = i+1; j < str.Length; j++)
+                                    {
+                                        if (arr[i]%2!=0 && arr[j]%2==0)
+                                        {
+                                           arr[i]=arr[i]+arr[j];
+                                            arr[j]=arr[i]-arr[j];
+                                            arr[i] = arr[i] - arr[j];
+                                            count++;
+                                            break;
+                                        }
+                                    }
+                                    Console.WriteLine(count);
+                                }
+                                Console.WriteLine(count);
+                            }*/
+                    
+                }
+                if ((even != odd))
+                    Console.WriteLine(-1);
+                else
+                    Console.WriteLine(even);
+
+            }
+        }
+    }
+
+ }
+
+        
+    
+
---
+++ C-Sharp/Problem-1529A/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_1529A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int TestCase=Convert.ToInt32(Console.ReadLine());
+            while (TestCase-- > 0)
+            {
+                int avg ,ct=0;
+                int sum = 0;
+                int min;
+                int num=Convert.ToInt32(Console.ReadLine());
+               String[] str1 =Console.ReadLine().Split();
+               int[] arr1 = new int[str1.Length];
+             // int[]arr=new int[num];    
+               for (int i = 0; i < str1.Length; i++)
+                  //  for (int i = 0; i < num; i++)
+                    {
+                      arr1[i]=Convert.ToInt32(str1[i]);
+                  //  arr[i] = Convert.ToInt32(arr[i]);
+                }
+              //  for (int i = 0; i < str1.Length; i++)
+             //   {
+              //      sum=sum+arr1[i];
+                   // Console.WriteLine(sum / str1.Length);
+            //    }
+            min=arr1.Min();
+               avg= sum/str1.Length;
+                for (int i = 0; i < str1.Length; i++)
+                {
+                    if(min<arr1[i])
+                        ct++;
+                        
+                }
+                Console.WriteLine(ct);
+            }
+            //  Fun(arr,str.Length);
+            
+
+            }
+        }
+   /*    private static int Fun(int[] arr1,int l)
+
+        {
+            int avg=0;
+            for (int i = 0; i < l; i++)
+            {
+                avg += arr1[i];
+            }
+            return avg/l;
+        }*/
+    }
+
