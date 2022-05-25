0537c6ac75feb00c41a2bc279b4bbfa73462697e
Mynul Islam <mynulakash436@gmail.com>
2022-05-26 00:31:16 +0600

    .

--- C-Sharp/FileHandalingExist/Program.cs
+++ C-Sharp/FileHandalingExist/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+string match =@"E:\Dev Skill\Data.txt";
+
+if(File.Exists(match))
+{
+    Console.WriteLine("THE FILE EXIST..");
+    string path=File.ReadAllText(match);
+    Console.WriteLine(path);
+
+    string match2 = @"E:\Dev Skill\Data2.txt";
+    File.Copy(match, match2);
+}
+else
+{
+    Console.WriteLine("Does not exist");
+}
