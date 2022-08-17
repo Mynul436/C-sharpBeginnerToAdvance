c12010f328c796983b71b87778a18ae96be3093a
Mynul Islam <mynulakash436@gmail.com>
2022-08-17 16:01:54 +0600

    Hash Table

---
+++ C-Sharp/HashTable/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable h1 = new Hashtable() {
+                {"salary",4000 }
+            
+            };
+            h1.Add("id", 1234);
+            h1.Add("name", "Mynul");
+
+            Console.WriteLine(h1.Contains("name"));
+            Console.WriteLine(1234.GetHashCode());
+            foreach (var item in h1.Keys)
+            {
+                Console.WriteLine(item);
+                Console.WriteLine(h1[item]);
+            }
+            Console.WriteLine("1234".GetHashCode());
+            //  Console.WriteLine(h1["name"]);
+
+        }
+    }
+}
